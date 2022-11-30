38ac9f50110e991bb9514e6137d76e4cb22d9235
Harsh Krishna <87293901+harshkrishna17@users.noreply.github.com>
2022-11-30 13:23:17 +0530

    Update Data.R

--- Data.R
+++ Data.R
@@ -10,15 +10,21 @@ data2 <- tm_player_market_values(country_name = "Spain", start_year = 2022)
 data3 <- tm_player_market_values(country_name = "Germany", start_year = 2022)
 data4 <- tm_player_market_values(country_name = "Italy", start_year = 2022)
 data5 <- tm_player_market_values(country_name = "France", start_year = 2022)
+data6 <- tm_player_market_values(country_name = "Portugal", start_year = 2022)
+data7 <- tm_player_market_values(country_name = "Austria", start_year = 2022)
+data8 <- tm_player_market_values(country_name = "Netherlands", start_year = 2022)
+data9 <- tm_player_market_values(country_name = "Scotland", start_year = 2022)
+data10 <- tm_player_market_values(league_url = "https://www.transfermarkt.co.in/championship/startseite/wettbewerb/GB2",
+                                  start_year = 2022)
 
 # Combine and Wrangle
 
-data <- rbind(data1, data2, data3, data4, data5) 
-
+data <- rbind(data1, data2, data3, data4, data5, data6, data7, data8, data9, data10) 
+unique(data$comp_name)
 data <- data %>%
   filter(!player_name == "Benjamin Mendy") %>%
   filter(!player_name == "Mason Greenwood")
-  
+
 # Save
 
 write.csv(data, "CompositionSquadData.csv", row.names = FALSE)
